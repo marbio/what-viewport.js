847f43570d6c855fd62473c91534151aeda9713a
marbio <marbio17@gmail.com>
2014-08-04 16:12:55 +0200

    format script file

--- what-viewport.js
+++ what-viewport.js
@@ -1,123 +1,102 @@
-
 /*
  * @WhatViewport module
- *	
- * Determines the width of the viewport (cross-browser) and offers conditional methods 
- * To figure out which device we are usign.
- * 
+ *
+ * Determines the width of the viewport (cross-browser) and offers conditional methods
+  * To figure out which device we are usign.
+ *
  */
-(function () {
+(function() {
 
-    if (typeof $Modules === 'undefined') { $Modules = {} }
+	if ( typeof $Modules === 'undefined') {
+		$Modules = {}
+	}
 
-    $Modules.whatViewport = function () {
+	$Modules.whatViewport = function() {
 
-        var w = window,
-			a = 'inner',
-			ww = '',
-			wh = '',
-			mode = '', 
-			options = {
-				breakpoints : {
-					mobile : 767,
-					tabletLandscapeDown : 767,
-					tabletLandscapeUp : 1279,
-					tabletPortraitDown : 767,
-					tabletPortraitUp : 1001,
-					desktop : 1279,
-					desktopWide : 1407
-				},
-				mode: {
-					landscape : 'landscape',
-					portrait : 'portrait'
-				}
-				
-			};
-
-
-        var init = function( params ) {
-
-            if ( !( 'innerWidth' in window ) ) {
-				a = 'client';
-				w = document.documentElement || document.body;
+		var w = window, a = 'inner', ww = '', wh = '', mode = '', options = {
+			breakpoints : {
+				mobile : 767,
+				tabletLandscapeDown : 767,
+				tabletLandscapeUp : 1279,
+				tabletPortraitDown : 767,
+				tabletPortraitUp : 1001,
+				desktop : 1279,
+				desktopWide : 1407
+			},
+			mode : {
+				landscape : 'landscape',
+				portrait : 'portrait'
 			}
-	
-			ww = w[ a+'Width'];
-			wh = w[ a+'Height'];
-			
-			mode = getOrientation();
-        
-        },
-
-        getOrientation = function() {
-
-            if ( wh > ww )
-				mode = options.mode.portrait;
-			else
-				mode = options.mode.landscape;
-				
-			return mode;	
-
-        },
-        
-        isMobile = function () {
-
-			return ( ww <= options.breakpoints.mobile );
-
-		},
-
-		isTabletLandscape = function () {
-
-			return ( ww > options.breakpoints.mobile && ww < options.breakpoints.tabletLandscapeUp && mode == options.mode.landscape );
-
-		},
-
-		isTabletPortrait = function () {
-
-			return ( ww > options.breakpoints.mobile && ww < options.breakpoints.tabletPortraitUp && mode == options.mode.portrait );
-
-		},
-
-		isMobileLandscape = function () {
-
-			return ( ww < options.breakpoints.mobile && mode == options.mode.landscape );
-
-		},
-
-		isMobilePortrait = function () {
-
-			return ( ww < options.breakpoints.mobile && mode == options.mode.portrait );
-
-		},
-
-		isDesktop = function () {
-
-			return ( ww > options.breakpoints.desktop );
-
-		},
-
-		isWideDesktop = function () {
-
-			return ( ww > options.breakpoints.desktopWide );
 
 		};
 
-        var whatViewportObj = {
+		var init = function(params) {
 
-            init: init,
-            getOrientation : getOrientation,
-            isMobile: isMobile,
-            isMobilePortrait : isMobilePortrait,
-            isMobileLandscape : isMobileLandscape,            
-            isTabletLandscape : isTabletLandscape,
-            isTabletPortrait : isTabletPortrait,
-            isDesktop: isDesktop,
-            isWideDesktop: isWideDesktop
+			if (!('innerWidth' in window )) {
+				a = 'client';
+				w = document.documentElement || document.body;
+			}
 
-        }; 
+			ww = w[a + 'Width'];
+			wh = w[a + 'Height'];
 
-        return whatViewportObj;
+			mode = getOrientation();
 
-    }();
+		}, getOrientation = function() {
 
-})();
+			if (wh > ww)
+				mode = options.mode.portrait;
+			else
+				mode = options.mode.landscape;
+
+			return mode;
+
+		}, isMobile = function() {
+
+			return (ww <= options.breakpoints.mobile );
+
+		}, isTabletLandscape = function() {
+
+			return (ww > options.breakpoints.mobile && ww < options.breakpoints.tabletLandscapeUp && mode == options.mode.landscape );
+
+		}, isTabletPortrait = function() {
+
+			return (ww > options.breakpoints.mobile && ww < options.breakpoints.tabletPortraitUp && mode == options.mode.portrait );
+
+		}, isMobileLandscape = function() {
+
+			return (ww < options.breakpoints.mobile && mode == options.mode.landscape );
+
+		}, isMobilePortrait = function() {
+
+			return (ww < options.breakpoints.mobile && mode == options.mode.portrait );
+
+		}, isDesktop = function() {
+
+			return (ww > options.breakpoints.desktop );
+
+		}, isWideDesktop = function() {
+
+			return (ww > options.breakpoints.desktopWide );
+
+		};
+
+		var whatViewportObj = {
+
+			init : init,
+			getOrientation : getOrientation,
+			isMobile : isMobile,
+			isMobilePortrait : isMobilePortrait,
+			isMobileLandscape : isMobileLandscape,
+			isTabletLandscape : isTabletLandscape,
+			isTabletPortrait : isTabletPortrait,
+			isDesktop : isDesktop,
+			isWideDesktop : isWideDesktop
+
+		};
+
+		return whatViewportObj;
+
+	}();
+
+})(); 
